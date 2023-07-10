fd4ca966f67940d36e76696bc12b3ab1a70120bc
kessie <kayp3477@gmail.com>
2023-07-10 12:46:46 +0000

    Add social media component with various icons

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BsTwitter, BsLinkedin, BsGithub } from 'react-icons/bs';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <BsTwitter />
+    </div>
+    <div>
+      <BsGithub />
+    </div>
+    <div>
+      <BsLinkedin />
+    </div>
+  </div>
+);
+
+export default SocialMedia;
