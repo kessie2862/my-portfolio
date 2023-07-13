52b644e87bbe79ce9b670366106b0683ea866bfe
kessie <kayp3477@gmail.com>
2023-07-13 14:04:13 +0000

    Changed welcome emoji

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
       >
         <div className="app__header-badge">
           <div className="badge-cmp app__flex">
-            <span>👋</span>
+            <span></span>
             <div style={{ marginLeft: 20 }}>
               <p className="p-text">Hi, my name is</p>
               <h1 className="head-text peekay">Prosper</h1>
