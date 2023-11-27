d7b2799844a2f163b30c9efe8bc16df8debc0694
kessie <kayp3477@gmail.com>
2023-11-27 17:31:31 +0000

    Fix image URL

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -33,7 +33,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <img src={urlFor(about.imgUrl).url()} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
